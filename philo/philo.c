7f0be8d1a9c297db2bb159f06fe83974121b9fbf
Youngseon Ahn <yoahn@c3r4s6.42seoul.kr>
2021-06-22 16:14:20 +0900

    edit

--- philo/philo.c
+++ philo/philo.c
@@ -2,5 +2,16 @@
 
 int		main(int argc, char *argv[])
 {
-	
+	if (argc < 5 || argc > 6)
+	{
+		printf("argument error\n");
+		return (0);
+	}
+	printf("number of philosophers: %s\n", argv[1]);
+	printf("time to die: %s\n", argv[2]);
+	printf("time to eat: %s\n", argv[3]);
+	printf("time to sleep: %s\n", argv[4]);
+	if (argc == 6)
+		printf("number of times each philosopher must eat: %s\n", argv[5]);
+	return (0);
 }
